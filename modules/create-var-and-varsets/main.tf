347503d601d662dab4b1d0cf2e6002c00ce98a6b
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2026-07-13 23:41:13 +0530

    Update main.tf

--- modules/create-var-and-varsets/main.tf
+++ modules/create-var-and-varsets/main.tf
@@ -4,17 +4,17 @@
 locals {
   tfc_org_name = "Test-Abhinav"
 }
-data "tfe_organization" "databricks" {
+data "tfe_organization" "tfeorg" {
   name = local.tfc_org_name
 }
 #
 data "tfe_oauth_client" "vcs_client" {
-organization = data.tfe_organization.databricks.name
+organization = data.tfe_organization.tfeorg.name
 service_provider = "github"
 name = "Github-dryrun"
 }
 output "org_name" {
-value = data.tfe_organization.databricks.name
+value = data.tfe_organization.tfeorg.name
 }
 
 output "tfe_oauth_client_vcs_client_oauth_token_id" {
@@ -26,45 +26,45 @@ data "tfe_project" "projname" {
   organization = local.tfc_org_name
 }
 
-resource "tfe_variable_set" "databricks_account_values_var_set" {
+resource "tfe_variable_set" "account_values_var_set" {
 
-  name         = "databricks_account"
-  description  = "Databricks Acccount level variables"
-  organization = data.tfe_organization.databricks.name
+  name         = "test_account"
+  description  = "Acccount level variables"
+  organization = data.tfe_organization.tfeorg.name
 }
 
-resource "tfe_variable" "databricks_account_id" {
+resource "tfe_variable" "account_id" {
 
-  key             = "databricks_account_id"
+  key             = "daccount_id"
   value           = "0adadfb6-215e-4626-a676-c7ed67144454"
   category        = "terraform"
-  description     = "The databricks account id value"
-  variable_set_id = tfe_variable_set.databricks_account_values_var_set.id
+  description     = "The account id value"
+  variable_set_id = tfe_variable_set.account_values_var_set.id
 }
 
-resource "tfe_variable" "databricks_base_account_url" {
+resource "tfe_variable" "base_account_url" {
 
-  key             = "databricks_base_account_url"
-  value           = "https://accounts.cloud.databricks.com"
+  key             = "base_account_url"
+  value           = "https://accounts.cloud.com"
   category        = "terraform"
-  description     = "The databricks account base url value"
-  variable_set_id = tfe_variable_set.databricks_account_values_var_set.id
+  description     = "account base url value"
+  variable_set_id = tfe_variable_set.account_values_var_set.id
 }
-resource "tfe_variable" "databricks_client_id" {
+resource "tfe_variable" "client_id" {
 
   key             = "client_id"
   value           = "testclientid"
   category        = "terraform"
-  description     = "The databricks spi client id value"
-  variable_set_id = tfe_variable_set.databricks_account_values_var_set.id
+  description     = "The spi client id value"
+  variable_set_id = tfe_variable_set.account_values_var_set.id
 }
 
-resource "tfe_variable" "databricks_client_secret" {
+resource "tfe_variable" "client_secret" {
 
   key             = "client_secret"
   value           = "testsecret"
   category        = "terraform"
-  description     = "The databricks spi client secret value"
-  variable_set_id = tfe_variable_set.databricks_account_values_var_set.id
+  description     = "The  spi client secret value"
+  variable_set_id = tfe_variable_set.account_values_var_set.id
   sensitive       = true
 }

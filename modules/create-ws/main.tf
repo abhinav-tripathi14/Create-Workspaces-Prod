a05ad18fc47a2b3f3b32c3896e2dcca0a1cdde76
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2026-07-13 23:13:16 +0530

    Create main.tf

---
+++ modules/create-ws/main.tf
@@ -0,0 +1,96 @@
+locals {
+  tfc_org_name = "Test-Abhinav"
+}
+data "tfe_organization" "databricks" {
+  name = local.tfc_org_name
+}
+#
+data "tfe_oauth_client" "vcs_client" {
+  organization = data.tfe_organization.databricks.name
+service_provider = "github"
+}
+output "org_name" {
+value = data.tfe_organization.databricks.name
+}
+
+output "tfe_oauth_client_vcs_client_oauth_token_id" {
+  value = data.tfe_oauth_client.vcs_client.oauth_token_id
+}
+
+data "tfe_project" "projname" {
+  name         = "Dry-Run"
+  organization = local.tfc_org_name
+}
+
+resource "tfe_workspace" "ws_workspace" {
+
+  name         = "Workspace-1" # each.value.ws_name
+  organization = local.tfc_org_name
+  description  = "New databricks workspace" 
+  tag_names = ["databricks-workspace"]
+  working_directory = "databricks-workspaces-new"
+  auto_apply        = true
+  terraform_version = "~>1.8.0"
+
+  project_id          = data.tfe_project.projname.id
+  global_remote_state = false
+
+  vcs_repo {
+    identifier     = "abhinav-tripathi14/Create-Workspaces-Prod"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.vcs_client.oauth_token_id
+  }
+  trigger_patterns = ["databricks-workspaces-new/*", "modules/*"]
+
+  lifecycle {
+    ignore_changes = [
+      auto_apply
+    ]
+  }
+}
+
+###########################################################
+# Variable Set creation for Databricks Values
+###########################################################
+resource "tfe_variable_set" "databricks_account_values_var_set" {
+
+  name         = "databricks_account"
+  description  = "Databricks Acccount level variables"
+  organization = data.tfe_organization.databricks.name
+}
+
+resource "tfe_variable" "databricks_account_id" {
+
+  key             = "databricks_account_id"
+  value           = "0adadfb6-215e-4626-a676-c7ed67144454"
+  category        = "terraform"
+  description     = "The databricks account id value"
+  variable_set_id = tfe_variable_set.databricks_account_values_var_set.id
+}
+
+resource "tfe_variable" "databricks_base_account_url" {
+
+  key             = "databricks_base_account_url"
+  value           = "https://accounts.cloud.databricks.com"
+  category        = "terraform"
+  description     = "The databricks account base url value"
+  variable_set_id = tfe_variable_set.databricks_account_values_var_set.id
+}
+resource "tfe_variable" "databricks_client_id" {
+
+  key             = "client_id"
+  value           = "testclientid"
+  category        = "terraform"
+  description     = "The databricks spi client id value"
+  variable_set_id = tfe_variable_set.databricks_account_values_var_set.id
+}
+
+resource "tfe_variable" "databricks_client_secret" {
+
+  key             = "client_secret"
+  value           = "testsecret"
+  category        = "terraform"
+  description     = "The databricks spi client secret value"
+  variable_set_id = tfe_variable_set.databricks_account_values_var_set.id
+  sensitive       = true
+}
